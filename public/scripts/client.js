1129863edbbb2eccee73e2b433a6b7017ace7c86
Rukia Sheikh-mohamed <rukiasheikh-mohamed@macbook-2.local>
2017-01-25 13:18:06 -0600

    completed title, author, and publication_date

---
+++ public/scripts/client.js
@@ -0,0 +1,47 @@
+//client side logic
+$(function(){
+  console.log('document loaded');
+
+  getBooks();
+  //listen for a submit event on the form
+  $('#book-form').on('submit', addBook);
+});
+function getBooks(){
+  $.ajax({
+    url: '/books',
+    type: 'GET',
+    success: displayBooks
+  });
+}
+
+function displayBooks(books){
+  console.log('Got books from the server', books);
+
+  $('book-list').empty();
+  books.forEach(function(book){
+    //can't create in ht eDOM because you're not sure how much we need from the DB
+    var $li = $('<li></li>');
+    $li.append('<p><strong>' + book.title + '</strong></p>');
+    $li.append('<p><em>' + book.author + '</em></p>');
+    var date = new Date(book.publication_date).toDateString();
+    $li.append('<p><time>' + date + '</time></p>');
+
+    $('#book-list').append($li);
+  });
+
+}
+function  addBook(event){
+  //prevent browser from refreshing
+  event.preventDefault();
+//get the infor out of the form
+//jQuery method to get info out of form
+var formData = $(this).serialize();
+//send data to server
+$.ajax({
+  url: '/books',
+  type: 'POST',
+  data: formData,
+  success: getBooks
+})
+
+}
---
+++ routes/books.js
@@ -0,0 +1,86 @@
+// same as var router = require('express').Router();
+ var express = require('express');
+var router = express.Router();
+
+var pg = require('pg');
+//we need to provide the database info so it knows where to connect to
+
+var config = {
+  database: 'library'
+  //string called library connect to that database.
+};
+//way to create connection to database. connect and talk to database
+//use new for constructor functions so it sets things up correctly
+//initialize connection POOL. think of as 'how I connect to DB'
+var pool = new pg.Pool(config);
+
+router.get('/', function (req,res){
+  //you can put SQL quaries into JS
+
+
+  //err-an error onject, will be non-null if there was some error connecting to DB
+  //  ex DB not running, confiq is incorrect
+
+  //client-use to make actual quaries against DB
+
+  //done-- function to call when we are finished
+  //           returns connection back to the pool.
+  pool.connect(function(err, client, done){
+    if (err){
+      console.log('Error connecting to DB', err);
+      res.sendStatus(500);
+      done();
+    } else {
+        //no error occurred, time to query
+
+        // 1. sql string--the actual SQL query we want to running
+        // 2.callback --function to run after the database gives us our result
+        //       takes an error onject and the result object as it's args
+        client.query('SELECT * FROM books ORDER BY title;', function(err, result){
+          //waiting for database to get information back
+          done();
+          if(err) {
+            console.log('Error querying DB', err);
+            res.sendStatus(500);
+          }else{
+            console.log('Got info from DB', result.rows);
+            res.send(result.rows);
+          }
+
+        })
+
+    }
+  });
+});
+router.post('/', function(req, res){
+  pool.connect(function(err, client, done){
+    if (err){
+      console.log('Error connecting to DB', err);
+      res.sendStatus(500);
+      done();
+    } else {
+        //no error occurred, time to query
+
+        // 1. sql string--the actual SQL query we want to running
+        // 2. array of data--any data we want to pass to a parameterized statement
+        // 3..callback --function to run after the database gives us our result
+        //       takes an error onject and the result object as it's args
+        client.query('INSERT INTO books (title, author, publication_date, edition, publisher) VALUES ($1, $2, $3, $4, $5) RETURNING *;',
+        [req.body.title, req.body.author, req.body.published, req.body.edition, req.body.publisher],
+        function(err, result){
+          //waiting for database to get information back
+          done();
+          if(err) {
+            console.log('Error querying DB', err);
+            res.sendStatus(500);
+          }else{
+            console.log('Got info from DB', result.rows);
+            res.send(result.rows);
+          }
+
+        })
+
+    }
+  });
+});
+module.exports = router;
